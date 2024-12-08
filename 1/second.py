80c1061fe272a901a707654f52ce4758af4f5cc1
PrzemyslawSalek <psalek1999@gmail.com>
2024-12-08 12:06:30 +0100

    file input

--- 1/second.py
+++ 1/second.py
@@ -1,29 +1,30 @@
 list1 = []
 counter = {}
 
-while True:
-    line = input()
-    if not line:
-        break
-    num = list(map(int, line.split()))
+with open("input.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+        num = list(map(int, line.split()))
 
-    count1 = 0
-    if len(list1) == 0:
-        list1.insert(0, num[0])
-    else:
-        for idx, x in enumerate(list1):
-            if x >= num[0]:
-                break
-            count1+=1
-        list1.insert(count1, num[0])
+        count1 = 0
+        if len(list1) == 0:
+            list1.insert(0, num[0])
+        else:
+            for idx, x in enumerate(list1):
+                if x >= num[0]:
+                    break
+                count1 += 1
+            list1.insert(count1, num[0])
 
-    if num[1] in counter:
-        counter[num[1]]+=1
-    else:
-        counter[num[1]] = 1
+        if num[1] in counter:
+            counter[num[1]] += 1
+        else:
+            counter[num[1]] = 1
 
 result = 0
 for idx, x in enumerate(list1):
-    result = result + (x * counter.get(x, 0))
+    result += x * counter.get(x, 0)
 
 print(result)
